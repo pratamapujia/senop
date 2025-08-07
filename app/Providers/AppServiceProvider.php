dddc80d911d6aefdd83cc3ddf61f2b387c5c9b90
pratamapujia <pujiistiani123@gmail.com>
2025-08-07 10:40:00 +0700

    up app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,5 +21,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::defaultView('vendor.pagination.mypagination');
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
